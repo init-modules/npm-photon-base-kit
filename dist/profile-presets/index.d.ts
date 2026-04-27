3f94baf0dd4f77e30be78d8f18b6527f476b1989
Vlad <vladislavkadatskikh6@gmail.com>
2026-04-28 01:33:00 +0500

    chore: tighten .gitignore and untrack build artifacts

--- dist/profile-presets/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { MarketingDemoDesignTemplate, MarketingDemoDesignTemplateId, MarketingDemoProfileStarterPreset, baseDesignTemplates, baseProfileStarterPresets, createBaseDesignTemplateDocument, createBaseProfileDocumentFromPresetSource, createBaseProfileDocumentFromTemplateSource } from '../documents.js';
-import '@init/photon/server';
-import '../design-presets-j2te0UgP.js';
-import 'react';
--- dist/public.d.ts
+++
@@ -1,6 +0,0 @@
-import { PhotonInstallableKit, PhotonModule } from '@init/photon/public';
-
-declare const basePublicPhotonModule: PhotonModule;
-declare const basePublicPhotonKit: PhotonInstallableKit;
-
-export { basePublicPhotonKit, basePublicPhotonModule };
